4f244d5b8fcb61ce298c178cf9638a117007ad31
nodari-dev <nodari.pylypyshak@gmail.com>
2024-11-30 21:04:07 +0100

    .

--- main.go
+++ main.go
@@ -7,23 +7,38 @@ import (
 	_ "image/png"
 	"log"
 	"os"
+	// ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
-var arr = [10]byte{' ',  '`', '.', ',' ,'*', '~', '+', '&', '#', '@'}
+// var arr = []byte{' ', '`', '.', ',', '*', '~', '+', '&', '#', ' '}
+var arr = []byte{' ', '`', '.', ',', '*', '~', '+', '&', '#', '@'}
 
 func main() {
-	file, err := os.Open("robert.png")
-	defer file.Close()
+
+	list, err := os.ReadDir("images")
 	if err != nil {
-		log.Fatal(err)
+		panic("WHAATT")
 	}
 
-	img, _, err := image.Decode(file)
-	if err != nil {
-		log.Fatal(err)
+	for _, entry := range list {
+		if !entry.IsDir() {
+			file, err := os.Open("images/" + entry.Name())
+			defer file.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+			img, _, err := image.Decode(file)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			render(img)
+		}
 	}
 
+}
 
+func render(img image.Image) {
 	orig_bounds := img.Bounds().Max
 
 	scale_x := orig_bounds.X / 110
@@ -32,13 +47,21 @@ func main() {
 	new_img_x := orig_bounds.X / scale_x
 	new_img_y := orig_bounds.Y / scale_y
 
-	for y := range new_img_y{
-		for x := range new_img_x{
-			r, g, b, _ := img.At(x * scale_x, y * scale_y).RGBA()
+	fmt.Print("\033[H\033[J")
+	for y := range new_img_y {
+		for x := range new_img_x {
+			r, g, b, _ := img.At(x*scale_x, y*scale_y).RGBA()
 			lum := (19595*r + 38470*g + 7471*b + 1<<15) >> 24
-			indx := lum * 10 / 256
-			color := fmt.Sprintf("\033[38;2;%d;%d;%dm%c\033[0m", uint8(r), uint8(g), uint8(b), arr[indx])
-			fmt.Print(color)
+			indx := lum * uint32(len(arr)) / 256
+			var cell string
+			// if indx == uint32(len(arr)) - 1 {
+			// 	cell = fmt.Sprintf("\033[48;2;%d;%d;%dm%c\033[0m", uint8(r), uint8(g), uint8(b), arr[indx])
+			// } else{
+			// 	cell = fmt.Sprintf("\033[38;2;%d;%d;%dm%c\033[0m", uint8(r), uint8(g), uint8(b), arr[indx])
+			// }
+			
+			cell = fmt.Sprintf("\033[38;2;%d;%d;%dm%c\033[0m", uint8(r), uint8(g), uint8(b), arr[indx])
+			fmt.Print(cell)
 		}
 		fmt.Println()
 	}
